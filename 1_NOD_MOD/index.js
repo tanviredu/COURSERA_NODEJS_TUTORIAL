6c054feb54e080121434114bdf934ab5e3e1e332
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2020-08-16 13:41:12 +0600

    node callbacks and error handling

--- 1_NOD_MOD/index.js
+++ 1_NOD_MOD/index.js
@@ -2,14 +2,19 @@
 var rect = require("./rectangle");
 
 function solveRect(l,b){
-    console.log(`Solving for rectangle with l = ${l} and b = ${b} `);
-    if(l <= 0 || b <=0){
-        console.log("Dimenstion must be greater than zero");
-    }else{
-        console.log("Area of the rectangle is "+rect.area(l,b));
-        console.log("Peremeter of the rectangle "+rect.perimeter(l,b));
+        rect(l,b,(err,rectangle)=>{
+           if(err){
+               // error object has a message property
+               console.log(err.message);
+           } else{
+               console.log(`The area ${rectangle.area(l,b)}`);
+               console.log(`The perimeter ${rectangle.perimeter(l,b)}`);
+
+           }
+        });
+        console.log("this line is executed early because of async");
     }
-}
+
 
 solveRect(2,4);
 solveRect(3,5);
--- 1_NOD_MOD/rectangle.js
+++ 1_NOD_MOD/rectangle.js
@@ -1,3 +1,23 @@
 // creating a node modules
-exports.perimeter = (x,y)=>(2*(x+y));
-exports.area = (x,y)=>(x*y);
+// third parameter is actually a function
+// this will be called when the work is done
+// the callback first parameter is error
+// second is result [if there is no error]
+module.exports = (x,y,callback) =>{
+    if(x<=0 || y<=0 ){
+        setTimeout(()=>callback(new Error("ERROR : Enter valid value"),null),2000);
+
+    }else{
+        setTimeout(()=>callback(null,{
+            perimeter : (x,y)=>(2*(x+y)),
+            area : (x,y)=>(x*y)
+        }),2000);
+
+    }
+}
+
+
+
+
+
+
